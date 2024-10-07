884607b723925465c5c0d00a54459bf36adaba9a
Christian.KRAFT <chriskraft272@gmail.com>
2024-10-07 16:36:46 +0200

    date fixed in db

--- src/app/edit-dialog/edit-dialog.component.ts
+++ src/app/edit-dialog/edit-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Reservation} from "../domain/Reservation";
+import {ReservationService} from "../../service/reservation.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-dialog',
@@ -8,5 +12,48 @@ import { Component } from '@angular/core';
   styleUrl: './edit-dialog.component.css'
 })
 export class EditDialogComponent {
+  private dialogRef: MatDialogRef<EditDialogComponent> = inject(MatDialogRef<EditDialogComponent>)
 
+  private pass: Reservation = inject(MAT_DIALOG_DATA)
+
+  private toUpdate: Reservation;
+
+  constructor(private reservationService: ReservationService) {
+    this.toUpdate = this.pass;
+  }
+
+  ngOnInit(): void {
+    this.toUpdate = this.pass;
+  }
+
+  dateUpdateForm = new FormGroup({
+    date: new FormControl<Date>(new Date(this.pass.date), [
+      Validators.required
+    ])
+  })
+
+
+  onSubmit() {
+    //this.toUpdate.fromTime = this.dateUpdateForm.controls.fromDate.value;
+    this.updateReservation();
+    this.dialogRef.close();
+
+  }
+
+
+  updateReservation() {
+    this.toUpdate.date = this.dateUpdateForm.get('date').value.toUTCString();
+    this.reservationService.updateReservation(this.toUpdate);
+  }
+
+  //updates form if the user changes the input
+  updateFormDate(value: any) {
+    this.dateUpdateForm.get('date').setValue(value);
+    this.dateUpdateForm.controls.date.value.setHours(3)
+    this.dateUpdateForm.controls.date.value.setMinutes(1  )
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
--- src/app/form-dialog/form-dialog.component.ts
+++ src/app/form-dialog/form-dialog.component.ts
@@ -1,12 +1,109 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ReservationService} from "../../service/reservation.service";
+import {MatButton} from "@angular/material/button";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import {MatFormField} from "@angular/material/form-field";
+import {MatCard} from "@angular/material/card";
+import {MatCalendar, MatDatepickerInput} from "@angular/material/datepicker";
+import {provideNativeDateAdapter} from "@angular/material/core";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {Reservation} from "../domain/Reservation";
+import {DialogData} from "../domain/DialogData";
+
 
 @Component({
-  selector: 'app-form-dialog',
+  selector: 'app-popup',
   standalone: true,
-  imports: [],
   templateUrl: './form-dialog.component.html',
+  imports: [
+    ReactiveFormsModule,
+    MatButton,
+    MatDialogTitle,
+    MatDialogContent,
+    MatFormField,
+    MatDialogActions,
+    MatCard,
+    MatCalendar,
+    MatDatepickerInput,
+    MatTabGroup,
+    MatTab
+  ],
+  providers: [provideNativeDateAdapter()],
   styleUrl: './form-dialog.component.css'
 })
-export class FormDialogComponent {
+export class FormDialogComponent implements OnInit {
 
+  private reservationService: ReservationService = inject(ReservationService);
+
+  private dialogRef: MatDialogRef<FormDialogComponent> = inject(MatDialogRef<FormDialogComponent>);
+
+  private snackBar: MatSnackBar = inject(MatSnackBar);
+
+  private pass: DialogData = inject(MAT_DIALOG_DATA)
+
+  public disabled: Date[] = []
+
+  ngOnInit(): void {
+    //  console.log("INIT")
+    //console.log(this.pass.reservations)
+
+    for (let reservation of this.pass.reservations) {
+      this.disabled.push(new Date(reservation.date));
+    }
+
+    console.log(this.disabled)
+  }
+
+  dateInputForm = new FormGroup({
+    date: new FormControl<Date>(null, [
+      Validators.required
+    ])
+  })
+
+
+  onSubmit() {
+    //   console.log("submit")
+    console.log(this.dateInputForm.controls.date.value + "AT SUBMIT")
+    this.makeReservation(this.dateInputForm.controls.date.value, +this.pass.floor, this.pass.deskID);
+    this.dialogRef.close();
+
+  }
+
+
+  makeReservation(date: Date, floor: number, deskID: number) {
+    //console.log(!this.dateInputForm.valid)
+    //console.log(this.dateInputForm.get('fromDate').value.toLocaleDateString())
+    this.reservationService.makeReservation(date, floor, deskID)
+
+  }
+
+  updateFormDate(value: any) {
+    this.dateInputForm.get('date').setValue(value);
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  openSnackBar() {
+    this.snackBar.open("Reserved", "", {
+      duration: 5000
+    });
+  }
+
+  //function to disable dates that are already reserved
+  disabledDates = (d: Date): boolean => {
+    //d.setTime(new Date().getTime())
+    const time = d.getDate();
+    console.log(!this.disabled.find(x => x.getDate() == time));
+    return !this.disabled.find(x => x.getDate() == time);
+  }
 }

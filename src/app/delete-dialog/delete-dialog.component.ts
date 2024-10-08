fe6c586dc907dc6771726b2129f2973093d39dad
Martin.ZWICKLHUBER <martin.zwicklhuber@lbs4.salzburg.at>
2024-10-08 09:28:07 +0200

    create delete-dialog

--- src/app/delete-dialog/delete-dialog.component.ts
+++ src/app/delete-dialog/delete-dialog.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCalendar} from "@angular/material/datepicker";
+import {MatCard} from "@angular/material/card";
+import {MAT_DIALOG_DATA, MatDialogContent, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {ReactiveFormsModule} from "@angular/forms";
+import {Reservation} from "../domain/Reservation";
+import {ReservationService} from "../../service/reservation.service";
 
 @Component({
   selector: 'app-delete-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatCalendar,
+    MatCard,
+    MatDialogContent,
+    ReactiveFormsModule,
+    MatDialogModule
+  ],
   templateUrl: './delete-dialog.component.html',
   styleUrl: './delete-dialog.component.css'
 })
 export class DeleteDialogComponent {
 
+  private dialogRef: MatDialogRef<DeleteDialogComponent> = inject(MatDialogRef<DeleteDialogComponent>)
+
+  private pass: Reservation = inject(MAT_DIALOG_DATA)
+
+  private reservationService = inject(ReservationService)
+
+
+  delete() {
+    this.reservationService.deleteReservation(this.pass)
+    this.closeDialog()
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
